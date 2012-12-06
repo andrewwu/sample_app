1a3af4cd0a6823e06e7be9aec6624caf5a7a3748
Andrew Wu <wujuichang@gmail.com>
2012-12-05 22:41:34 -0800

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
-FactoryGirl.define do 
+FactoryGirl.define do
   factory :user do
-    name     "Andrew Wu"
-    email    "andrew@example.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
